4d3b288702ea5d05ae21a613259ab233a777d72d
Matthew Hartley <matthewh@ebi.ac.uk>
2022-11-24 20:23:04 +0000

    Refactor IO code to file all files

--- bia_explorer/io.py
+++ bia_explorer/io.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 from imageio import imread_v2
 from PIL import Image
 
-from bia_explorer.biostudies import load_submission, find_file_lists_in_submission, flist_from_flist_fname
+from bia_explorer.biostudies import load_submission, find_files_in_submission
 
 
 FILE_TEMPLATE = "https://www.ebi.ac.uk/biostudies/files/{accession_id}/{file_relpath}"
@@ -48,12 +48,13 @@ class BIAStudy(BaseModel):
 def load_bia_study(accession_id: str) -> BIAStudy:
     submission = load_submission(accession_id)
     
-    file_list_fnames = find_file_lists_in_submission(submission)
-    file_lists = [flist_from_flist_fname(accession_id, fname) for fname in file_list_fnames]
-    fl = file_lists[0]
+    # file_list_fnames = find_file_lists_in_submission(submission)
+    # file_lists = [flist_from_flist_fname(accession_id, fname) for fname in file_list_fnames]
+    # fl = file_lists[0]
 
+    files = find_files_in_submission(submission)
     
-    image_files = [file for file in fl if is_image(file)]
+    image_files = [file for file in files if is_image(file)]
     
     def get_image_uri(file):
         return FILE_TEMPLATE.format(accession_id=accession_id, file_relpath=file.path)
